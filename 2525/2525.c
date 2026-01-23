3f3010a14619e4171fb207cad16feaaf6c33652b
세언 <seeon4143@gmail.com>
2026-01-23 15:18:24 +0900

    Update 2525.c

--- 2525/2525.c
+++ 2525/2525.c
@@ -9,7 +9,7 @@ int main() {
     scanf("%d",&c);
 
         
-    printf("%d %d\n",(a+((b+c)/60))%24,(b+c)%60);
+    printf("%d %d\n",(a+((b+c)/60))%24,(b+c)%60); // b분이랑 c분을 더해서 시간, 분 출력함.
         
     
     return 0;
